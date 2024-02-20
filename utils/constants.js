0768ead7efe4228932e2a69939079995f286129d
Eason <lisen8018@gmail.com>
2024-02-20 22:33:09 +0800

    change contract address

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
 import abi from './Lottery.json'
 export const lotteryAbi = abi.abi
-export const LotteryAddress = '0x73AEf6823B9F7E125593e58b8CC72098ddCb1869'
+export const LotteryAddress = '0x29c7a982a0Ea75b824b16B0C92DaeFc3D3f48b48'
