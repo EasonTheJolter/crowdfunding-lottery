9a406cf51292af93cc67c7ab3b1d9655cab40107
Eason <lisen8018@gmail.com>
2024-02-21 15:20:22 +0800

    read ticket price from chain

--- components/TableRow.js
+++ components/TableRow.js
@@ -1,11 +1,13 @@
 import style from '../styles/TableRow.module.css'
 import truncateEthAddress from 'truncate-eth-address'
+import { useAppContext } from '../context/context'
 const TableRow = ({ player }) => {
+  const { ticketPrice } = useAppContext()
   return (
     <div className={style.wrapper}>
       <div className={style.address}>{truncateEthAddress(player)}</div>
       <div className={style.ethAmmount}>
-        <span className={style.goldAccent}>+0.015 ETH</span>
+        <span className={style.goldAccent}>+{ticketPrice} JOLT</span>
       </div>
     </div>
   )
