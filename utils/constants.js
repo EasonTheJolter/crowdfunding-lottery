b7a4511dbae03edbc215ff39509af4a32463eb98
Eason <lisen8018@gmail.com>
2024-02-21 15:21:59 +0800

    add chainInfo

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,7 @@
 import abi from './Lottery.json'
 export const lotteryAbi = abi.abi
-export const LotteryAddress = '0x29c7a982a0Ea75b824b16B0C92DaeFc3D3f48b48'
+export const LotteryAddress = '0x0b733F3E7874035e08a392F3473D648157068877'
+export const chainInfo = {
+  chainId: '0x61',
+  chainName: 'BSC Testnet'
+}
