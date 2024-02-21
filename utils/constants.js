1b9ca06eadcb6a76128c4223dd09ac731652c15e
Eason <lisen8018@gmail.com>
2024-02-21 17:00:47 +0800

    change chain into jolt-evm

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 import abi from './Lottery.json'
 export const lotteryAbi = abi.abi
-export const LotteryAddress = '0x0b733F3E7874035e08a392F3473D648157068877'
+export const LotteryAddress = '0x2BCD5745eBf28f367A0De2cF3C6fEBfE42B21338'
 export const chainInfo = {
-  chainId: '0x61',
-  chainName: 'BSC Testnet'
+  chainId: '0x6c2',
+  chainName: 'Joltify-EVM'
 }
